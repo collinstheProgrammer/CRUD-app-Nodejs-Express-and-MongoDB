11704d4572b7acc18b7289103b71282c58358d9a
donnyllionaire <collinstheprogreammer@gmail.com>
2019-09-07 01:21:01 +0300

    using res.sendFile() instead of res.render() function expressJS

--- Controllers/employeeController.js
+++ Controllers/employeeController.js
@@ -1,8 +1,16 @@
 const express = require('express')
-var router = express.Router()
+const path = require('path')
+const router = express.Router()
 
-router.get('/',(req,res)=>{
-  res.json('sample text')
-})
+//router.get('/',function(req,res){
+  //res.render('employee/addOrEdit',{viewTitle:"Register New Employee"})
+//res.sendFile('/home/donnyllionaire/CRUD-app-Nodejs-Express-and-MongoDB/employee/addOrEdit.html',{viewTitle:"Register New Employee"})
+//res.render('addOrEdit')
+
+//  res.json('sample text')
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/home/donnyllionaire/CRUD-app-Nodejs-Express-and-MongoDB/employee/addOrEdit.html'));
+  //__dirname : It will resolve to your project folder.
+});
 //exporting the Router
 module.exports = router
--- server.js
+++ server.js
@@ -4,23 +4,37 @@ require('./models/db')
 const express = require('express')
 const employeeController = require('./Controllers/employeeController')
 const path = require('path')
-consr exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const router = express.Router()
+
 //calling the express function
 var app = express()
 
 //configuring the views
-app.set('views', path.join(__dirname, '/views/layouts/'))
+//app.set('views', path.join(__dirname, '/views/'))
 
 //configuring the express engine for handle bars
-app.engine('hbs', exphbs({extname:'hbs', defaultlayout: 'mainLayout',
-layouDir:__dirname + '/view/layouts/mainLayout
-app.set('view engine','hbs');
-}))
+//app.engine('hbs', exphbs({extname:'hbs', defaultlayout: 'mainLayout', layoutsDir:__dirname + '/view/layouts/mainLayout'}))
+//app.engine('hbs', require('hbs').__express)
+//app.engine('hbs', require('addOrEdit').renderFile);
+
+//app.engine(app.engine('html', require('ejs').renderFile))
+//app.set('view engine','hbs');
+//app.set('views',__dirname + '/views')
 
 ///starting the server
+
+
+//app.use('/employee', employeeController)
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/addOrEdit.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use('/',router)
 app.listen(3000, ()=>{
   console.log('Express server started at port:3000')
 })
 
 //configure the routing for the application
-app.use('/employee', employeeController)
